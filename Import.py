2a622e7e9258cf1d80493985ccd7b078ca76f9b3
bnymnsntrk <bnymnsntrk@gmail.com>
2020-05-03 00:58:03 +0300

    Decision Tree added

--- Import.py
+++ Import.py
@@ -5,9 +5,6 @@ from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
 from sklearn.metrics import accuracy_score, confusion_matrix, make_scorer, precision_recall_fscore_support, roc_auc_score
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import RobustScaler, StandardScaler
-
-import pandas as pd
-import numpy as np
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
@@ -23,6 +20,7 @@ from sklearn import metrics
 from matplotlib import pyplot as plt
 from sklearn.utils import shuffle
 from sklearn.model_selection import GridSearchCV, StratifiedKFold, train_test_split
+from sklearn.tree import DecisionTreeClassifier
 
 
 data = pd.read_csv("data.csv")              #read data
@@ -33,7 +31,10 @@ training_length = len(data)*0.7             #%70 of data is training data
 x = data[['fLength', 'fWidth', 'fSize', 'fConc', 'fConc1', 'fAsym', 'fM3Long', 'fM3Trans', 'fAlpha', 'fDist']]      #features
 y = data['class']                                                                                                   #label
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)       #train
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1)       #train
+
+
+############### Naive Bayes ##################
 
 classifier = GaussianNB()
 classifier.fit(x_train, y_train)
@@ -42,10 +43,17 @@ y_pred = classifier.predict(x_test)
 accuracy = accuracy_score(y_test, y_pred)*100
 print(accuracy)
 
-#features = np.array(pd.DataFrame(data, columns=['fLength', 'fWidth', 'fSize', 'fConc', 'fConc1', 'fAsym', 'fM3Long', 'fM3Trans', 'fAlpha', 'fDist']))
-#label = np.array(pd.DataFrame(data, columns=['class']))
+############### Naive Bayes ##################
 
 
-#model.fit(features, label.ravel())
-#predicted = model.predict([[48.2468, 17.3565, 3.0332, 0.2529, 0.1515, 8.573, 38.0957, 10.5868,4.792, 219.087]])
-#print("Predicted Value:", predicted)
+############### Decision Tree ##################
+
+classifier2 = DecisionTreeClassifier()
+classifier2 = classifier2.fit(x_train, y_train)
+
+y_pred2 = classifier2.predict(x_test)
+accuracy2 = accuracy_score(y_test, y_pred2)*100
+print(accuracy2)
+
+############### Decision Tree ##################
+
